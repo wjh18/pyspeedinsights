3145ac969a6f34ba9ebbc57d73ffe85011df3cd9
Will Holmes <willholmes95@gmail.com>
2022-11-01 23:49:43 -0400

    Add more tests for generic utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,10 @@
 import pytest
 
+from pyspeedinsights.utils.generic import (
+    remove_dupes_from_list,
+    remove_nonetype_dict_items,
+    sort_dict_alpha,
+)
 from pyspeedinsights.utils.urls import validate_url
 
 
@@ -41,3 +46,32 @@ class TestValidateUrl:
         url = "https://example.com/hello?test=test&test2=test2"
         mod_url = validate_url(url)
         assert mod_url == url.split("?")[0]
+
+
+class TestDictUtils:
+    dct = {1: None, 2: None, 3: "test"}
+    dct2 = {"test": 1, "zest": 2, "lest": 3, "rest": 4}
+
+    def test_none_is_removed(self):
+        mod_dct = remove_nonetype_dict_items(self.dct)
+        assert None not in mod_dct.values()
+
+    def test_sort_dict_alphabetically(self):
+        s_dct = sort_dict_alpha(self.dct2)
+        assert list(s_dct.keys()) == ["lest", "rest", "test", "zest"]
+
+
+class TestDedupeList:
+    s = 1
+    d = 2
+    md = 3
+    lst = [s] * s + [d] * d + [md] * md
+
+    def test_single_occurences_are_not_removed(self):
+        assert remove_dupes_from_list(self.lst).count(self.s) == self.s
+
+    def test_duplicates_are_removed(self):
+        assert remove_dupes_from_list(self.lst).count(self.d) == self.s
+
+    def test_multiple_duplicates_are_removed(self):
+        assert remove_dupes_from_list(self.lst).count(self.md) == self.s
