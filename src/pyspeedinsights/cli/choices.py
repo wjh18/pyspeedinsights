c77961464d4d2091610bbabec9505b2bff1a7283
Will Holmes <willholmes95@gmail.com>
2023-07-05 23:36:58 -0400

    Remove metrics from command choices

--- src/pyspeedinsights/cli/choices.py
+++ src/pyspeedinsights/cli/choices.py
@@ -2,7 +2,6 @@
 
 `category`, `strategy` and `locale` are based on available PSI API query params.
 `format` is specific to pyspeedinsights and dictates the output format of the results.
-`metrics` defines available performance metrics to include. `all` collects all metrics.
 """
 
 COMMAND_CHOICES = {
@@ -51,49 +50,4 @@ COMMAND_CHOICES = {
         "vi",
     ),
     "format": ("json", "excel", "sitemap"),
-    # `all` is used as an arg to include all metrics in the Excel output
-    # This must be a list instead of tuple so "all" can be removed from it
-    "metrics": [
-        "all",
-        "observedCumulativeLayoutShift",
-        "observedLargestContentfulPaintAllFrames",
-        "maxPotentialFID",
-        "observedSpeedIndexTs",
-        "observedFirstContentfulPaintTs",
-        "observedTimeOrigin",
-        "observedFirstPaint",
-        "observedNavigationStartTs",
-        "observedLargestContentfulPaintAllFramesTs",
-        "speedIndex",
-        "observedFirstContentfulPaint",
-        "observedLastVisualChangeTs",
-        "cumulativeLayoutShiftMainFrame",
-        "observedLastVisualChange",
-        "cumulativeLayoutShift",
-        "largestContentfulPaint",
-        "observedDomContentLoaded",
-        "firstContentfulPaint",
-        "observedCumulativeLayoutShiftMainFrame",
-        "observedFirstVisualChange",
-        "observedFirstPaintTs",
-        "totalCumulativeLayoutShift",
-        "observedFirstMeaningfulPaint",
-        "interactive",
-        "observedTraceEnd",
-        "observedFirstMeaningfulPaintTs",
-        "totalBlockingTime",
-        "observedFirstContentfulPaintAllFramesTs",
-        "observedLargestContentfulPaint",
-        "observedNavigationStart",
-        "observedLoad",
-        "observedFirstVisualChangeTs",
-        "observedFirstContentfulPaintAllFrames",
-        "observedTimeOriginTs",
-        "observedTraceEndTs",
-        "observedLoadTs",
-        "observedDomContentLoadedTs",
-        "observedSpeedIndex",
-        "firstMeaningfulPaint",
-        "observedLargestContentfulPaintTs",
-    ],
 }
